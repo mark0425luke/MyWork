491055519b12491b993d3734996d7d3a8b4f9658
mark0425luke <mark0425luke@gmail.com>
2024-03-08 01:44:16 +0800

    (1) 把 Add and Distributor 的 OR 加進去, 改了 config, main, energy, area (2) main 最後加上 #Macro 統計

--- Compression_and_Parallelize/OU_Table.py
+++ Compression_and_Parallelize/OU_Table.py
@@ -5,6 +5,7 @@ import Config
 import math
 import logging
 import pickle
+import MyWork
 logger = logging.getLogger()
 
 def sort_the_key_of_each_cluster(dictionary_list):
@@ -110,23 +111,32 @@ def get_OU_table_parameter(OU_table, mywork):
     
     MAX_NUM_FILTER = 0
     at_least_num_Macro = int( math.ceil( len(OU_table) / (mywork.num_OU_per_Macro)) ) # 也就是 (幾個 shape) / (一個Macro多少Ou * 一個 Tile 多少 Macro)
+    num_cluster_types_for_each_OU_shape = []
+    sum_of_num_cluster_types_for_each_OU_shape = 0
     num_input_output_for_each_OU_shape = []
     sum_of_num_input_output_for_each_OU_shape = 0
     num_OU_shape = len(OU_table)
     for key, value in OU_table.items(): # key 是 OU 的 shape, value 是 (input cluster->output vector)
         
+
         # logger.info(f"key = {key}")
         # logger.info(f"value = {value}")
         # ex.
         # shape 1 : (cluster1 -> ....), (cluster2 -> ....), (cluster4 -> ....)
-        # shape 1 : (cluster1 -> ....), (cluster3 -> ....), (cluster4 -> ....), (cluster5 -> ....)
+        # shape 2 : (cluster1 -> ....), (cluster3 -> ....), (cluster4 -> ....), (cluster5 -> ....)
         # 則 num_input_for_each_OU = [3,4]
         # 而沒有 bitline 壓縮的話，可能就會變
         # ex. OU=2
-        # shape 1 : (cluster1 -> v4, v5), (cluster1 -> v8, v50), (cluster2 -> v3, v6), (cluster4 -> v1, v7), (cluster4 -> ....), (cluster4 -> ....)....
-        # shape 1 : (cluster1 -> ....), (cluster1 -> ....), (cluster3 -> ....), (cluster3 -> ....), (cluster3 -> ....), (cluster4 -> ....), (cluster5 -> ....)
-        # 則 num_input_for_each_OU = [6, 7]
+        # shape 1 : (cluster1 -> v4, v5), (cluster1 -> v8, v50), (cluster2 -> v3, v6), (cluster4 -> v1, v7), (cluster4 -> ....), (cluster4 -> ....)
+        # shape 2 : (cluster1 -> ....), (cluster1 -> ....), (cluster3 -> ....), (cluster3 -> ....), (cluster3 -> ....), (cluster4 -> ....), (cluster5 -> ....)
+        # 則 num_types_cluster_for_each_OU_shape  = [3, 4]
+        # 則 num_input_output_for_each_OU_shape = [6, 7]
         num_input_output_for_each_OU_shape.append(len(value))
+        what_kind_of_cluster = set()
+        for item in value:
+            what_kind_of_cluster.add(tuple(item[0])) # item[0] 是 cluster, item[1] 是 output vectors
+        num_cluster_types_for_each_OU_shape.append(len(what_kind_of_cluster))
+        
 
         
         # ex.BIT_W=8, BIT_PER_CELL = 1
@@ -152,26 +162,158 @@ def get_OU_table_parameter(OU_table, mywork):
                 MAX_NUM_FILTER = tmp_NUM_FILTER if(tmp_NUM_FILTER > MAX_NUM_FILTER) else MAX_NUM_FILTER
 
     sum_of_num_input_output_for_each_OU_shape = sum(num_input_output_for_each_OU_shape)
-    return MAX_NUM_FILTER, at_least_num_Macro, num_input_output_for_each_OU_shape, sum_of_num_input_output_for_each_OU_shape, num_OU_shape
+    sum_of_num_cluster_types_for_each_OU_shape = sum(num_cluster_types_for_each_OU_shape)
+    return MAX_NUM_FILTER, at_least_num_Macro, num_OU_shape,\
+        num_cluster_types_for_each_OU_shape, sum_of_num_cluster_types_for_each_OU_shape,\
+        num_input_output_for_each_OU_shape, sum_of_num_input_output_for_each_OU_shape
 
 
-def get_PE_OU_cycle_for_each_OU_shape(layer_idx, OU, PE_idx):
 
-    # 存 PE_cycle
-    file_name   = "/home/mark/MyWork_OU2/PE_OU_cycle_for_each_OU_shape/" \
-                    + "CONV" + str(layer_idx+1) \
-                    + '/OU=' + str(mywork.OU) \
-                    + '_num_Macro_per_Tile=' + str(Config.num_Macro_per_Tile)\
-                    + '_PE' + str(PE_idx) \
-                    + '.pickle'
-    with open(file_name, 'rb') as file:
-        PE_OU_cycle_for_each_OU_shape = pickle.load(file) 
+# ===============================================================
+# 純粹為了不要引用 main 才在這邊建的 class
+# =============================================================== 
+class MyWork():
+    def __init__(self, OU):
+        self.OU = OU
+        self.num_OU_per_Macro       = int( math.ceil( (Config.XB_paramX/self.OU)**2 ) )
+ 
+        self.ADC_PRECISION          = (Config.BIT_PER_CELL + Config.BIT_DAC + math.log(self.OU, 2)) if (Config.BIT_PER_CELL!=1 and Config.BIT_DAC!=1) \
+                                        else (Config.BIT_PER_CELL + Config.BIT_DAC + math.log(self.OU, 2)-1)
 
+
+        self.PE_MAX_NUM_FILTER      = [ [0 for j in range(Config.NETWORK_DICT["K"][i])] for i in range(Config.NETWORK_DICT["total_layer_num"])]
+        self.Tile_MAX_NUM_FILTER    = [ 0 for j in range(Config.NETWORK_DICT["total_layer_num"])]
+
+
+        self.PE_num_Macro   = [ [0 for j in range(Config.NETWORK_DICT["K"][i])] for i in range(Config.NETWORK_DICT["total_layer_num"])]
+        self.num_Tile               = [ 0 for i in range(Config.NETWORK_DICT["total_layer_num"])] # 這個之後 Activation, Pooling, Rowbuffers 需要
+
+        self.num_PE_OU_shape        = [  [0 for j in range(Config.NETWORK_DICT["K"][i])] for i in range(Config.NETWORK_DICT["total_layer_num"])]
+        self.sum_of_PE_num_input_output_for_each_OU_shape = [  [0 for j in range(Config.NETWORK_DICT["K"][i])] for i in range(Config.NETWORK_DICT["total_layer_num"])]
+
+        self.PE_time                = [[0 for i in range(Config.NETWORK_DICT["K"][i])] for i in range(Config.NETWORK_DICT["total_layer_num"])]
+        self.Tile_time              = [ 0 for i in range(Config.NETWORK_DICT["total_layer_num"])] 
+        
 
         
 
-    print(f"CONV{layer_idx+1}, OU={OU}, PE_idx={PE_idx}, PE_OU_cycle_for_each_OU_shape = {PE_OU_cycle_for_each_OU_shape}")
+        # self.DUPLICATE              = [ 1 for i in range(Config.NETWORK_DICT["total_layer_num"])] 
+        self.bottleneck_layer_idx = None
+        self.BOTTLENECK_LATENCY     = 0 # 單位 : cycle
+        self.total_pipeline_latency = None
+
+
+
+        self.conv_area_model        = None
+        self.conv_energy_model      = None
+
+        self.pooling_area_Model     = None
+        self.pooling_energy_Model   = None
+
+
+        self.router_area_model      = None
+        self.router_energy_model    = None
+
+    def get_parameters(self):
+        # print(f"mywork parameters : ")
+        # print(f"        OU = {self.OU}")
+        # print(f"        num_OU_per_Macro = {self.num_OU_per_Macro}")
+        # print(f"        ADC_PRECISION = {self.ADC_PRECISION}")
+        # print(f"        PE_MAX_NUM_FILTER = {self.PE_MAX_NUM_FILTER}")
+        # print(f"        Tile_MAX_NUM_FILTER = {self.Tile_MAX_NUM_FILTER}")
+        # print(f"        PE_at_least_num_Tile = {self.PE_at_least_num_Tile}")
+        # print(f"        num_Tile = {self.num_Tile}")
+        # print(f"        PE_time = {self.PE_time}")
+        # print(f"        Tile_time = {self.Tile_time}")
+        # print(f"        DUPLICATE = {self.DUPLICATE}")
+        # print(f"        BOTTLENECK_LATENCY = {self.BOTTLENECK_LATENCY}")
+
+        logging.info(f"mywork parameters : ")
+        logging.info(f"        OU = {self.OU}")
+        logging.info(f"        num_OU_per_Macro = {self.num_OU_per_Macro}")
+        logging.info(f"        ADC_PRECISION = {self.ADC_PRECISION}")
+        logging.info(f"        PE_MAX_NUM_FILTER = {self.PE_MAX_NUM_FILTER}")
+        logging.info(f"        Tile_MAX_NUM_FILTER = {self.Tile_MAX_NUM_FILTER}")
+        logging.info(f"        PE_num_Macro = {self.PE_num_Macro}")
+        logging.info(f"        num_Tile = {self.num_Tile}")
+        logging.info(f"        num_PE_OU_shape = {self.num_PE_OU_shape}")
+        logging.info(f"        sum_of_PE_num_input_output_for_each_OU_shape = {self.sum_of_PE_num_input_output_for_each_OU_shape}")
+        logging.info(f"        PE_time = {self.PE_time}")
+        logging.info(f"        Tile_time = {self.Tile_time}")
+        # logging.info(f"        DUPLICATE = {self.DUPLICATE}")
+        logging.info(f"        BOTTLENECK_LATENCY = {self.BOTTLENECK_LATENCY}")
+        logging.info(f"        total_pipeline_latency = {self.total_pipeline_latency}")
+############################################
+
+
+
 
 if __name__=='__main__':
-    print("haha")
-    get_PE_OU_cycle_for_each_OU_shape(layer_idx=12, OU=4, PE_idx=0)
+    
+    # ===============================================================
+    # 設定
+    # =============================================================== 
+    layer_idx = 10
+    PE_idx = 0
+    ############################################
+
+
+
+    # ===============================================================
+    # 讀取 mywork
+    # =============================================================== 
+    # 讀 pickle
+    file_name   =  Config.home_dir + "mywork_after_parallelize_OU_to_Macro/" \
+                + 'OU=' + str(Config.OU) + '_BIT_PER_CELL=' + str(Config.BIT_PER_CELL) \
+                + '_required_LATENCY=' + str(Config.LATENCY*Config.CLK_PERIOD/1e+06) + 'ms'\
+                + '.pickle'
+    print(f"load : {file_name}")
+    with open(file_name, 'rb') as file:
+        mywork = pickle.load(file)  
+    ############################################
+    
+
+    
+    
+    # ===============================================================
+    # 讀取 OU table
+    # =============================================================== 
+    
+    file_name   = Config.home_dir + "OU_table_FullReuse_without_bitline_compression/" \
+                + "OU_table_CONV" + str(layer_idx+1) \
+                + '_OU=' + str(Config.OU) + '_BIT_PER_CELL=' + str(Config.BIT_PER_CELL) \
+                + '_PE' + str(PE_idx) \
+                + '.pickle'
+    print(f"load : {file_name}")
+    try:
+        # Open the pickle file in binary read mode
+        with open(file_name, 'rb') as file:
+            # Load the data from the pickle file
+            PE_OU_table = pickle.load(file)
+    # Now, `loaded_OU_table` contains the Python object stored in the pickle file
+    except FileNotFoundError:
+        print(f"File not found: {file_name}")
+    except EOFError:
+        print("Error: Ran out of input while unpickling (possibly empty or corrupted pickle file).")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    ############################################
+
+
+    print(f"PE_OU_table key is= {PE_OU_table.keys()}")
+    
+    # ===============================================================
+    # 印出參數
+    # =============================================================== 
+    PE_MAX_NUM_FILTER, PE_at_least_num_Macro, PE_num_OU_shape,\
+    num_cluster_types_for_each_OU_shape, sum_of_num_cluster_types_for_each_OU_shape,\
+    num_input_output_for_each_OU_shape, sum_of_num_input_output_for_each_OU_shape = get_OU_table_parameter(PE_OU_table, mywork)
+    
+    print(f"PE_MAX_NUM_FILTER   =   {PE_MAX_NUM_FILTER}")
+    print(f"PE_at_least_num_Macro   =   {PE_at_least_num_Macro}")
+    print(f"PE_num_OU_shape =   {PE_num_OU_shape}")
+    print(f"num_cluster_types_for_each_OU_shape =   {num_cluster_types_for_each_OU_shape}")
+    print(f"sum_of_num_cluster_types_for_each_OU_shape  =   {sum_of_num_cluster_types_for_each_OU_shape}")
+    print(f"num_input_output_for_each_OU_shape  =   {num_input_output_for_each_OU_shape}")
+    print(f"sum_of_num_input_output_for_each_OU_shape   =   {sum_of_num_input_output_for_each_OU_shape}")
+    ############################################
